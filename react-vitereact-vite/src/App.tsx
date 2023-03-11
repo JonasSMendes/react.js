77fe9e088d897102aafa56a7920cd88ec933b344
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-11 10:14:14 -0300

    expressões no react

--- react-vitereact-vite/src/App.tsx
+++ react-vitereact-vite/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+const app = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  let n1:number = 345
+  let n2:number = 26
+  let name:string = 'jonas'
+  let sobrenome:string = 'mendes'
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR jonas
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+  function somar(n1:number, n2:number):number{
+    return n1 * n2
+  }
+
+  return( 
+  <div>
+  a multiplicação do valor {n1} com o valor de {n2} é de {somar(n1,n2)} logo {`${name} ${sobrenome}`} estão com {somar(n1,n2)} peras
     </div>
   )
 }
 
-export default App
+export default app;
