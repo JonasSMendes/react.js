3baeecea1f68bc9551ca4b7c3585ed6d832ac6aa
jonas mendes <jonas.jsm2903@gmail.com>
2023-03-19 10:34:40 -0300

    instalando o tailwind de forma correta

--- react_imc/src/App.tsx
+++ react_imc/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css'
+import powerimg from './assets/img/powered.png'
 
 const App =()=>{
   return(
-    <div>
-          
+    <div className="">
+        <header>
+          <div className="max-w-screen-lg mt-6">
+              <img className='' src={powerimg} alt="" width={150}/>
+          </div>
+        </header>
     </div>
   )
 }
