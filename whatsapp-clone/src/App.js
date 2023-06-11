c7b9a93d53c384b8ebfe873cb17156a382a3984b
jonas mendes <jonas.jsm2903@gmail.com>
2023-06-11 07:14:52 -0300

    side bar criando

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,11 +1,58 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import ChatlistItem from './componts/ChatlistItem';
+
+
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
+
 function App() {
+
+  const [chatlist, setChatlist] = useState([{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {}]);
+
+
   return (
     <div className="App">
-          ola putas
+      <div className='side-bar'>
+        <header>
+          <img className='header-avater' src='https://i.pinimg.com/originals/e7/63/0c/e7630cb0fcfade92e4d98834053741e1.png' alt='' />
+          <div className='header-button'>
+            <div className='header-btn'>
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+            <div className='header-btn'>
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+            <div className='header-btn'>
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+          </div>
+        </header>
+
+        <div className='search'>
+          <div className='search-input' >
+            <SearchIcon fontSize='small' style={{ color: '#919191' }} />
+            <input type='search' placeholder='Procurar ou começar uma nova conversa' />
+          </div>
+        </div>
+
+        <div className='chat-list'>
+          {chatlist.map((item, key)=>(
+            <ChatlistItem key={key}/>
+          ))}
+        </div>
+
+      </div>
+      <div className='content-area'>
+        ....
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ whatsapp-clone/src/componts/ChatlistItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './ChatlistItem.css'
+
+export default () => {
+    return (
+        <div className="chatlist-item">
+            <img className="chatlist-avatar" src="https://i.pinimg.com/originals/b4/1a/04/b41a04050d7f84ee62d88f64484399ba.png" />
+            <div className="chatlist-item-lines">
+                <div className="chatlist-item-line">
+                    <div className="chatlist-item-name">
+                        jiji
+                    </div>
+                    <div className="chatlist-item-date">
+                        19:00
+                    </div>
+                </div>
+                <div className="chatlist-item-line">
+                    <div className="chatlist-item-lasting">
+                        <p>foi aquela sebosa eu juro</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
