63ee4751a1b524a37d5c95d2561044ee28b880a7
jonas mendes <jonas.jsm2903@gmail.com>
2023-06-28 14:13:34 -0300

    exercicio com tabela

--- react-next/src/components/tabela de notas/tabela.tsx
+++ react-next/src/components/tabela de notas/tabela.tsx
@@ -5,10 +5,43 @@ export const Tabela =()=>{
     
     return(
         <div>
-
+            <table border={1} color="#fff">
+                <tr>
+                <td>nome</td>
+                <td>status</td>
+                <td>grade 1</td>
+                <td>garde 2</td>
+                <td>final grade</td>
+                </tr>
             {students.map((item,index)=>(
-                <div></div>
+                <tr>
+                    <td>
+                        <div className="avatar-name">
+                            <img src={item.avatar}/>
+                            {item.name}
+                        </div>
+                        <div className="email">
+                            {item.email}
+                        </div>  
+                    </td>
+                    <td>
+                        {item.active &&
+                        <div style={{backgroundColor: 'green'}}>
+                            active
+                        </div>
+                        }
+                         {!item.active &&
+                        <div style={{backgroundColor: 'red'}}>
+                            inactive
+                        </div>
+                        }
+                    </td>
+                    <td>{item.grade1}</td>
+                    <td>{item.grade2}</td>
+                    <td>{((item.grade1+ item.grade2) /2).toFixed(1)}</td>
+                </tr>
             ))}
+            </table>
         </div>
     )
 } 
