6f2fc0bdbdb1bfe743a4a63e632b65c46d69fb90
jonas mendes <jonas.jsm2903@gmail.com>
2023-07-04 07:36:18 -0300

    editando primeira pagina

--- formulario-multi-etapas/src/app/FormStep1/page.tsx
+++ formulario-multi-etapas/src/app/FormStep1/page.tsx
@@ -1,13 +1,30 @@
+import Link from 'next/link'
 import * as S from './styled'
 
 import { Theme } from '@/components/theme'
 
 export default function FormStep1 (){
+
     return(
         <>
         <Theme>
              <S.Container>
-                passo 1
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>preencha o campo abaixo com o seu nome completo</p>
+
+                <hr />
+
+                <label htmlFor="">
+                    seu nome completo
+                    <input 
+                    type="text"
+                    autoFocus
+                    />
+                </label>
+
+                <Link href='/FormStep2'>Proximo</Link>
+
             </S.Container>
         </Theme>
            
--- formulario-multi-etapas/src/app/FormStep2/page.tsx
+++ formulario-multi-etapas/src/app/FormStep2/page.tsx
@@ -1,13 +1,16 @@
 'use client'
-
 import * as S from './styled'
+
+import { Theme } from '@/components/theme'
 
 export default function FormStep2 (){
     return(
         <>
-            <S.Container>
-                passo 2
-            </S.Container>
+            <Theme>
+                <S.Container>
+                    passo 2
+                </S.Container>
+            </Theme>
         </>
     )
 }
