e5022fed88f754be81b5a7a383410743c89c4463
jonas mendes <jonas.jsm2903@gmail.com>
2023-07-05 04:52:31 -0300

    criando tela principal

--- formulario-multi-etapas/src/app/FormStep1/page.tsx
+++ formulario-multi-etapas/src/app/FormStep1/page.tsx
@@ -1,15 +1,34 @@
 import Link from 'next/link'
 import * as S from './styled'
+import {useForm, FormAction} from '../../contexts/FormContext'
 
 import { Theme } from '@/components/theme'
+import { ChangeEvent, useEffect } from 'react';
 
 export default function FormStep1 (){
+
+    const {state, dispatch} = useForm();
+
+    useEffect(()=>{
+        dispatch({
+            type:FormAction.setCurrentStep,
+            payload: 1
+        })
+    },[])
+
+
+    const handleNameChange = (e : ChangeEvent<HTMLInputElement>) =>{
+        dispatch({
+            type: FormAction.setName,
+            payload: e.target.value
+        })
+    }
 
     return(
         <>
         <Theme>
              <S.Container>
-                <p>Passo 1/3</p>
+                <p>Passo {state.currentStep}/3 </p>
                 <h1>Vamos começar com seu nome</h1>
                 <p>preencha o campo abaixo com o seu nome completo</p>
 
@@ -20,6 +39,8 @@ export default function FormStep1 (){
                     <input 
                     type="text"
                     autoFocus
+                    value={state.name}
+                    onChange={handleNameChange}
                     />
                 </label>
 
